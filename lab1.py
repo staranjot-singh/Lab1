2fae15a49022c384db8b0c71a63cbd28ed4cd6ac
staranjot-singh <159674655+staranjot-singh@users.noreply.github.com>
2024-02-10 23:35:08 -0500

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,6 @@
+def calculate_age():
+    birth_year = int(input("Enter your birth year: "))
+    current_year = 2024  # Assuming the current year is 2024
+    age = current_year - birth_year 
+    print("your age is:",age)
+    
