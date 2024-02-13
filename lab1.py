6cf6bad5d45c54c9e6d0fb761de93b8c893e3683
staranjot-singh <159674655+staranjot-singh@users.noreply.github.com>
2024-02-13 00:23:49 +0000

    update

--- lab1.py
+++ lab1.py
@@ -2,4 +2,4 @@ try:
     age = int(input("Enter your age: "))
     print("Your age is:", age)
 except TypeError:
-    print("Please enter an integer for your age.")
+    print("Please enter an int.")
