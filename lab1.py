82d444140908605524387d40bcca5c3754a19b70
staranjot-singh <159674655+staranjot-singh@users.noreply.github.com>
2024-02-12 19:57:22 -0500

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,5 +1,10 @@
-try:
-    age = int(input("Enter your age: "))
-    print("Your age is:", age)
-except TypeError:
-    print("Please enter an int.")
+def calculate_age():
+    birth_year = int(input("Enter your birth year: "))
+    current_year = 2024  # Assuming the current year is 2024
+    age = current_year - birth_year 
+    print("your age is:",age)
+
+def helloWorld():
+    print('Hello World')
+
+helloWorld()
